478a5d9ccc34bb9477530ddaf6b71997dcc6d693
oraclecloud939 <oraclecloud939@gmail.com>
2025-05-15 03:09:14 +0200

    ok

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,4 @@
-kvm_default_network   = "default"
-windows_template_path = "/var/lib/libvirt/images/win10_template.qcow2"
+kvm_default_network   = "inside"
+windows_template_path = "/var/lib/libvirt/images/windows10-template.qcow2"
 
 # Valori specifici per le VM verranno definiti in main.tf
--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "kvm_default_network" {
 variable "windows_template_path" {
   type        = string
   description = "Percorso del template QCOW2 di Windows 10"
-  default     = "/var/lib/libvirt/images/windows10.qcow2"
+  default     = "/var/lib/libvirt/images/windows10-template.qcow2"
 }
 
 # Altre variabili globali se necessario
